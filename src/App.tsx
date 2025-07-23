56e89f10109cb586e6ca8861336751d70f6ee3bc
umyoonsang <kevin5459@naver.com>
2025-07-23 14:08:35 +0900

    충돌 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import MainLayout from "./components/layouts/MainLayout";
 import HomePage from "./pages/HomePage"; // HomePage import
 import NavBar from "./components/navbar/NavBar";
 import MyFormList from "@/pages/MyFormList"; // 경로에 맞게 조정
-import MyTakenList from "@/pages/MyTakenList"; // 경로에 맞게 조정
-import ResultPage from "@/pages/ResultPage"; // 경로에 맞게 조정
 import SignIn from "@/pages/auth/sign-in";
 import SignUp from "@/pages/auth/sign-up";
 import { NAVBAR_PADDING_TOP_CLASS } from "./constants/layout";
@@ -30,19 +28,9 @@ function App() {
             <Route path="/builder/:formId" element={<FormBuilderPage />} />
             {/* 설문 수정 경로 */}
             <Route path="/responses/:formId" element={<ResponsePage />} />
-            {/* 개별 폼 응답 조회 */}
-            <Route path="/list" element={<MyFormList />} />
-            {/* 내 설문(목록 페이지)*/}
-            <Route path="/results/:formId" element={<ResultPage />} />
-            {/* 결과확인 - 설문결과 */}
-
-            <Route path="/bookmarks" element={<MyTakenList />} />
-            {/* 참여한 설문(목록 페이지)*/}
-
+            <Route path="/my-forms" element={<MyFormList />} />
             <Route path="/sign-in" element={<SignIn />} />
-            {/* 로그인 */}
             <Route path="/sign-up" element={<SignUp />} />
-            {/* 회원가입 */}
           </Routes>
         </MainLayout>
       </main>
