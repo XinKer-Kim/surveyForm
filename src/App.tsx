be8fa64156c8188cade29313f10009c81c67f826
umyoonsang <kevin5459@naver.com>
2025-07-23 14:08:35 +0900

    충돌 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import FormBuilderPage from "./pages/FormBuilderPage";
+import ResponsePage from "./pages/ResponsePage";
+import MainLayout from "./components/layouts/MainLayout";
 import HomePage from "./pages/HomePage"; // HomePage import
-import SignIn from "./pages/auth/sign-in";
-import SignUp from "./pages/auth/sign-up";
-import { Toaster } from "./components/ui/sonner";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* 첫 화면을 HomePage로 변경 */}
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="sign-up" element={<SignUp />} />
-      </Routes>
-      <Toaster />
+      <NavBar />
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />{" "}
+          {/* 첫 화면을 HomePage로 변경 */}
+          <Route path="/builder/new" element={<FormBuilderPage />} />{" "}
+          {/* 새 설문 생성 경로 */}
+          <Route path="/builder/:formId" element={<FormBuilderPage />} />{" "}
+          {/* 설문 수정 경로 */}
+          <Route path="/responses/:formId" element={<ResponsePage />} />
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 }
