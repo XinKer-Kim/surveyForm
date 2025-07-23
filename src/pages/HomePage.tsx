2d8d1ee9aaefbf4862c6b3b9cad6c64e4229aafc
umyoonsang <kevin5459@naver.com>
2025-07-23 14:10:33 +0900

    홈페이지 수정 및 로그인 페이지생성 (#12)
    * 홈페이지 수정 및 로그인 페이지생성
    
    * 회원가입페이지 생성 중
    
    * 충돌 수정
    
    ---------
    
    Co-authored-by: XinKer-Kim <137377037+XinKer-Kim@users.noreply.github.com>
    
    윤상브랜치와 메인브랜치 충돌 리졸브

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,27 +2,26 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 function HomePage() {
   return (
-      <div className="page">
-        <div className="container">
-          <div className="flex flex-col gap-4 w-full max-w-[1055px] p-16">
-            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-              원하는 설문을 만들어보세요!
-            </h4>
-            {/* 템플릿 영역 */}
-            <div className="grid grid-cols-5 gap-6">
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-              <Skeleton className="w-full h-[170px] " />
-            </div>
+    <div className="page">
+      <div className="container">
+        <div className="flex flex-col gap-4 w-full max-w-[1000px]">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            원하는 설문을 만들어보세요!
+          </h4>
+          {/* 템플릿 영역 */}
+          <div className="w-full grid grid-cols-5 gap-6">
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
+            <Skeleton className="w-full h-[170px] bg-amber-500" />
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+function SignIn() {
+  return (
+    <div className="page">
+      <div className="container">
+        <div className="flex items-center justify-center pt-8">
+          <Card className="w-full max-w-sm">
+            <CardHeader>
+              <CardTitle>로그인</CardTitle>
+              <CardDescription>
+                로그인을 위한 정보를 입력해주세요.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">이메일</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="이메일을 입력하세요."
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">비밀번호</Label>
+                    </div>
+                    <Input
+                      id="password"
+                      type="password"
+                      placeholder="비밀번호를 입력하세요."
+                      required
+                    />
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+              <Button
+                type="submit"
+                variant="outline"
+                className="w-full cursor-pointer "
+              >
+                로그인
+              </Button>
+              <Button variant="outline" className="w-full cursor-pointer">
+                회원가입
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
