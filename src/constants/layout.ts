0a04780b92906570ca039b6a78101ba1bb8e20a1
DevGisoun <devgisoun@gmail.com>
2025-07-23 05:18:49 +0000

    [feat] MainLayout에 대해 NavBar 만큼의 상단 Padding 적용.
    - /contants/layout.ts 을 통한 NAVBAR_HEIGHT 상수 관리.

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,2 +1,2 @@
-export const NAVBAR_HEIGHT_CLASS = 'h-14';
-export const NAVBAR_PADDING_TOP_CLASS = 'pt-14';
+export const NAVBAR_HEIGHT_CLASS = "h-14";
+export const NAVBAR_PADDING_TOP_CLASS = "pt-14";
