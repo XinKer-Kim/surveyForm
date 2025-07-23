4df85a09d6ad47a2d2ccda0f5044465e852a64c7
umyoonsang <kevin5459@naver.com>
2025-07-23 13:59:12 +0900

    회원가입페이지 생성 중

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage"; // HomePage import
 import SignIn from "./pages/auth/sign-in";
+import SignUp from "./pages/auth/sign-up";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         {/* 첫 화면을 HomePage로 변경 */}
         <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   );
 }
