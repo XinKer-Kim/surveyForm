973023c556d8389ef43faba763a206171338098e
DevGisoun <devgisoun@gmail.com>
2025-07-23 01:39:17 +0000

    [feat] NavBar 작업 임시 저장 및 일부 import 수정.
    - FormBuilderPage: Link 컴포넌트 import
    - FC, ReactNode import 시 'type' 키워드 작성.
    - .gitignore: package-lock.json 추가.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import MainLayout from './components/layouts/MainLayout';
 import HomePage from './pages/HomePage'; // HomePage import
 import NavBar from './components/navbar/NavBar';
 import { NAVBAR_PADDING_TOP_CLASS } from './constants/layout';
+import SignIn from './pages/auth/sign-in';
+import SignUp from './pages/auth/sign-up';
 
 function App() {
   return (
@@ -12,18 +14,18 @@ function App() {
       <NavBar />
       <main className={NAVBAR_PADDING_TOP_CLASS}>
         <MainLayout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />{" "}
-          {/* 첫 화면을 HomePage로 변경 */}
-          <Route path="/builder/new" element={<FormBuilderPage />} />{" "}
-          {/* 새 설문 생성 경로 */}
-          <Route path="/builder/:formId" element={<FormBuilderPage />} />{" "}
-          {/* 설문 수정 경로 */}
-          <Route path="/responses/:formId" element={<ResponsePage />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </MainLayout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />{' '}
+            {/* 첫 화면을 HomePage로 변경 */}
+            <Route path="/builder/new" element={<FormBuilderPage />} />{' '}
+            {/* 새 설문 생성 경로 */}
+            <Route path="/builder/:formId" element={<FormBuilderPage />} />{' '}
+            {/* 설문 수정 경로 */}
+            <Route path="/responses/:formId" element={<ResponsePage />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+        </MainLayout>
       </main>
     </BrowserRouter>
   );
