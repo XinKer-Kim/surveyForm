02d5c6007bf88c83dcdf744db58b8d7183700d8f
DevGisoun <devgisoun@gmail.com>
2025-07-23 01:49:15 +0000

    [feat] Shadcn CSS 추가 및 전역 글꼴 설정.

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -11,13 +11,13 @@ function NavBar() {
       <header className="w-full flex flex-row items-center justify-center z-20 bg-white">
         <div className="w-full h-14 flex flex-row items-center justify-between px-8 font-bold">
           {/* '내 설문', '참여한 설문' 버튼 */}
-          <div className="flex flex-row items-center gap-4">
+          <div className="flex flex-row items-center gap-4 text-lg">
             <a href="/list">내 설문</a>
             <a href="/bookmarks">참여한 설문</a>
           </div>
           {/* '설문 만들기' 버튼 */}
           {pathname === '/list' ? (
-            <Button>
+            <Button className="font-semibold">
               <Plus />
               설문 만들기
             </Button>
