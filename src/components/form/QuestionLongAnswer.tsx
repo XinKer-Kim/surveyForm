640e97d8d36232e671221304211edb95ecbe47a2
XinKer-Kim <keyhoon.hg@gmail.com>
2025-07-23 11:40:16 +0900

    [feat] #10 주관식(단답/서술) 컴포넌트화

---
+++ src/components/form/QuestionLongAnswer.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { Textarea } from "@/components/ui/textarea";
+
+const QuestionLongAnswer: FC = () => {
+  return (
+    <Textarea
+      disabled
+      placeholder="참여자의 답변 입력란 (최대 2000자)"
+      maxLength={2000}
+      className="mt-2"
+    />
+  );
+};
+
+export default QuestionLongAnswer;
