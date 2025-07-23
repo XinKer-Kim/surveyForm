65e83300e5a1974583a284ac0f43ce7978418ad4
umyoonsang <kevin5459@naver.com>
2025-07-23 11:11:00 +0900

    홈페이지 수정 및 로그인 페이지생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FormBuilderPage from "./pages/FormBuilderPage";
-import ResponsePage from "./pages/ResponsePage";
-import MainLayout from "./components/layouts/MainLayout";
+
 import HomePage from "./pages/HomePage"; // HomePage import
+import SignIn from "./pages/auth/sign-in";
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />{" "}
-          {/* 첫 화면을 HomePage로 변경 */}
-          <Route path="/builder/new" element={<FormBuilderPage />} />{" "}
-          {/* 새 설문 생성 경로 */}
-          <Route path="/builder/:formId" element={<FormBuilderPage />} />{" "}
-          {/* 설문 수정 경로 */}
-          <Route path="/responses/:formId" element={<ResponsePage />} />
-        </Routes>
-      </MainLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* 첫 화면을 HomePage로 변경 */}
+        <Route path="sign-in" element={<SignIn />} />
+      </Routes>
     </BrowserRouter>
   );
 }
