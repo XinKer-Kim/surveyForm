ce58da4d825f24972123d83917fb85a374a89300
umyoonsang <kevin5459@naver.com>
2025-07-24 12:48:29 +0900

    [feat] 회원가입 유효성 검사 및 supabase연동 (#29)
    회원가입 유효성 검사 및 supabase연동 #29

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -14,57 +14,51 @@ import { Link } from "react-router";
 
 function SignIn() {
   return (
-    <div className="page">
-      <div className="container">
-        <div className="flex items-center justify-center pt-8">
-          <Card className="w-full max-w-sm">
-            <CardHeader>
-              <CardTitle>로그인</CardTitle>
-              <CardDescription>
-                로그인을 위한 정보를 입력해주세요.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="flex flex-col gap-6">
-                  <div className="grid gap-2">
-                    <Label htmlFor="email">이메일</Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="이메일을 입력하세요."
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    <div className="flex items-center">
-                      <Label htmlFor="password">비밀번호</Label>
-                    </div>
-                    <Input
-                      id="password"
-                      type="password"
-                      placeholder="비밀번호를 입력하세요."
-                      required
-                    />
-                  </div>
+    <div className="w-full flex items-center justify-center pt-8">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>로그인</CardTitle>
+          <CardDescription>로그인을 위한 정보를 입력해주세요.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">이메일</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="이메일을 입력하세요."
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">비밀번호</Label>
                 </div>
-              </form>
-            </CardContent>
-            <CardFooter className="flex-col gap-2">
-              <Button
-                type="submit"
-                variant="outline"
-                className="w-full cursor-pointer "
-              >
-                로그인
-              </Button>
-              <Link to={"/sign-up"} className="w-full cursor-pointer">
-                회원가입
-              </Link>
-            </CardFooter>
-          </Card>
-        </div>
-      </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="비밀번호를 입력하세요."
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button
+            type="submit"
+            variant="outline"
+            className="w-full cursor-pointer "
+          >
+            로그인
+          </Button>
+          <Link to={"/sign-up"} className="w-full cursor-pointer">
+            회원가입
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
