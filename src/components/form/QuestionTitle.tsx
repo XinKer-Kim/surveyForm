210dd79b81be3cf0ff46a97233872c10012b7d13
Dev Gisoun <devgisoun@gmail.com>
2025-07-24 09:30:31 +0900

    [feat] Shadcn UI - Dialog Comp 추가. (#27)
    * README
    
    * [chore] 프로젝트 설정 파일 수정.
    
    - .gitignore - .devcontainer/, .github/ 디렉토리 추가.
    - tsconfig.app.json - baseUrl, paths 프로퍼티 추가.
    
    * [feat] NavBar 작업 임시 저장 및 일부 import 수정.
    
    - FormBuilderPage: Link 컴포넌트 import
    - FC, ReactNode import 시 'type' 키워드 작성.
    - .gitignore: package-lock.json 추가.
    
    * [feat] Shadcn CSS 추가 및 전역 글꼴 설정.
    
    * 병합
    
    * [feat] NavBar 컴포넌트 보완.
    
    - 네이버 폼과 유사한 디자인 적용.
    - <a> 태그 대신 React Router의 <Link> 태그를 통한 Navigate 기능.
    
    * [feat] MainLayout에 대해 NavBar 만큼의 상단 Padding 적용.
    
    - /contants/layout.ts 을 통한 NAVBAR_HEIGHT 상수 관리.
    
    * [chore] Git 추적 목록에서 .env 파일 제거.
    
    * [feat] NavBar 작업 임시 저장 및 일부 import 수정.
    
    - FormBuilderPage: Link 컴포넌트 import
    - FC, ReactNode import 시 'type' 키워드 작성.
    - .gitignore: package-lock.json 추가.
    
    * [feat] Shadcn CSS 추가 및 전역 글꼴 설정.
    
    * [feat] 미완성 '설문 제목 입력' 컴포넌트.
    
    * App 라우터 통합 (#25)
    
    App 라우터 통합
    
    * [feat] Shadcn UI - Dialog Comp 추가.
    
    * [feat] Shadcn UI - Radio Group Comp 추가.
    
    * [chroe] '설문 제목 입력' 컴포넌트 제작 중.
    
    * [chore] 상수 값 직접 설정.
    
    ---------
    
    Co-authored-by: XinKer-Kim <137377037+XinKer-Kim@users.noreply.github.com>

--- src/components/form/QuestionTitle.tsx
+++ src/components/form/QuestionTitle.tsx
@@ -1,5 +1,17 @@
 import { Button } from '../ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog';
 import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
 
 interface QuestionTitleProps {
   handleAddInput: () => void;
@@ -9,15 +21,93 @@ interface QuestionTitleProps {
 function QuestionTitle({ handleAddInput, handleAddPage }: QuestionTitleProps) {
   return (
     <>
-      <div className="flex flex-col items-start border rounded-md p-4">
+      <div className="flex flex-col items-start border rounded-md p-4 gap-4">
         {/* '설문 제목 입력', '설명 입력', '설문 기간' 필드 컨테이너 */}
-        <div className="w-full flex flex-col">
+        <div className="w-full flex flex-col gap-2">
           <Input
             type="text"
-            className="p-0 border-x-0 border-t-0 border-b-2 border-transparent shadow-none rounded-none !text-lg font-bold placeholder:text-neutral-300 focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-black focus:border-naver"
+            className="p-0 border-x-0 border-t-0 border-b-2 border-transparent shadow-none rounded-none !text-lg font-bold placeholder:text-neutral-300 focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-neutral-300"
             placeholder="설문 제목 입력"
           />
-          <p>설명 입력</p>
+          <Input
+            type="text"
+            className="p-0 border-x-0 border-t-0 border-b-2 border-transparent shadow-none rounded-none !text-sm font-base placeholder:text-neutral-300 focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-neutral-300"
+            placeholder="설명 입력"
+          />
+          <div className="flex flex-row items-center justify-start gap-2 ">
+            <p className="w-20 text-xs">설문 기간</p>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Input
+                  type="text"
+                  readOnly
+                  onClick={() => console.log('ㅎㅇ')}
+                  className="focus-visible:ring-0 focus-visible:ring-offset-0"
+                />
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-xs [&>button]:hidden">
+                <DialogHeader>
+                  <DialogTitle className="flex justify-center py-1 text-base">
+                    설문 기간
+                  </DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-col items-start gap-6">
+                  <div className="flex flex-row gap-2">
+                    <Label className="pr-4">시작</Label>
+                    <RadioGroup
+                      defaultValue="comfortable"
+                      className="flex flex-row gap-6"
+                    >
+                      <div className="flex items-center gap-3">
+                        <RadioGroupItem value="start" id="r1" />
+                        <Label htmlFor="r1">즉시 시작</Label>
+                      </div>
+                      <div className="flex items-center gap-3">
+                        <RadioGroupItem value="custom" id="r2" />
+                        <Label htmlFor="r2">직접 설정</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    <Label className="pr-4">종료</Label>
+                    <RadioGroup
+                      defaultValue="comfortable"
+                      className="flex flex-row gap-6"
+                    >
+                      <div className="flex items-center gap-3">
+                        <RadioGroupItem value="noLimit" id="r1" />
+                        <Label htmlFor="r1">제한 없음</Label>
+                      </div>
+                      <div className="flex items-center gap-3">
+                        <RadioGroupItem value="custom" id="r2" />
+                        <Label htmlFor="r2">직접 설정</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                </div>
+                <DialogFooter className="sm:justify-start">
+                  <div className="w-full flex items-center justify-center gap-2">
+                    <DialogClose asChild>
+                      <Button
+                        type="button"
+                        className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded"
+                        onClick={handleAddPage}
+                      >
+                        취소
+                      </Button>
+                    </DialogClose>
+                    <Button
+                      type="button"
+                      className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded"
+                      onClick={handleAddPage}
+                    >
+                      확인
+                    </Button>
+                  </div>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
         </div>
         {/* '질문 추가', '페이지 추가' 버튼 컨테이너 */}
         <div className="w-full flex items-center justify-center gap-2">
