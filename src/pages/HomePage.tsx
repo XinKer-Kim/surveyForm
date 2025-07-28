875630ab4518d1bb4f428859e902604c880f7bd8
umyoonsang <kevin5459@naver.com>
2025-07-28 09:56:29 +0900

    브랜치 버전 맞추기

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,28 +11,24 @@ function HomePage() {
   };
 
   return (
-    <div className="page">
-      <div className="container">
-        <div className="flex flex-col gap-4 w-full max-w-[1000px]">
-          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-            원하는 설문을 만들어보세요!
-          </h4>
-          {/* 템플릿 영역 */}
-          <div className="w-full grid grid-cols-4 gap-4">
-            {templates.map((template) => (
-              <Card
-                key={template.id}
-                className="flex flex-col items-center justify-center gap-3 h-[170px] p-4 cursor-pointer hover:shadow-md"
-                onClick={() => handleSelectTemplate(template.id)}
-              >
-                {template.icon}
-                <div className="text-sm font-medium text-center">
-                  {template.label}
-                </div>
-              </Card>
-            ))}
-          </div>
-        </div>
+    <div className="flex flex-col gap-4 w-full max-w-[1100px]">
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        원하는 설문을 만들어보세요!
+      </h4>
+      {/* 템플릿 영역 */}
+      <div className="w-full flex flex-wrap gap-4">
+        {templates.map((template) => (
+          <Card
+            key={template.id}
+            className="flex flex-col items-center justify-center gap-3 w-[125px] h-[130px] p-4 cursor-pointer hover:shadow-md"
+            onClick={() => handleSelectTemplate(template.id)}
+          >
+            {template.icon}
+            <div className="text-sm font-medium text-center">
+              {template.label}
+            </div>
+          </Card>
+        ))}
       </div>
     </div>
   );
